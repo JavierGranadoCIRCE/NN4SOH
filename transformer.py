854c6e03d10c56b3acb726b69f054a54567a6755
Javier Granado <601354@unizar.es>
2025-02-18 14:01:37 +0100

    Commit nueva rama merge entre Dataset complet y entrenamiento Siames 18022025

--- transformer.py
+++ transformer.py
@@ -30,6 +30,7 @@ from SAnD.core.modules import ContrastiveLoss
 from SAnD.core.model import SAnD, SAnD_Embedding, SiameseSAnD
 from SAnD.utils.trainer import NeuralNetworkClassifier
 ######################################## Commit nueva rama merge entre Dataset complet y entrenamiento Siames 18022025
+######################################## Commit nueva rama merge entre Dataset complet y entrenamiento Siames 18022025
 
 # Real Dataset Generator
 #dataFile = 'dataset/ARC-FY/B0025'   # Modify this path
